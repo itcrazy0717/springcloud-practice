7dace3da0c05718695282efa7e0b5ddb133add6d
morewindows0 <morewindows@qq.com>
2020-04-05 21:04:13 +0800

    feat(master):新增模块，zk做配置中心

---
+++ springcloud-zk-practice/src/main/java/com/springcloud/zk/ZkPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkPracticeApplication.class, args);
+    }
+
+}
